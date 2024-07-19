8e18b61206a7a2ef75c4e9841d7af161191c7422
RickManMac <ricardo.lopes.macedo@gmail.com>
2024-07-19 17:49:57 +0100

    Butoes
    Adição de butoes close max, min

--- Calculadora.Designer.cs
+++ Calculadora.Designer.cs
@@ -31,20 +31,38 @@ namespace CalculadoraWindows
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.PnlTitle = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
             this.PnlHistory = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.PnlTitle.SuspendLayout();
             this.SuspendLayout();
             // 
             // PnlTitle
             // 
+            this.PnlTitle.Controls.Add(this.button2);
             this.PnlTitle.Controls.Add(this.button1);
+            this.PnlTitle.Controls.Add(this.BtnExit);
             this.PnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.PnlTitle.Location = new System.Drawing.Point(0, 0);
             this.PnlTitle.Margin = new System.Windows.Forms.Padding(0);
             this.PnlTitle.Name = "PnlTitle";
             this.PnlTitle.Size = new System.Drawing.Size(350, 40);
             this.PnlTitle.TabIndex = 0;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Image = ((System.Drawing.Image)(resources.GetObject("BtnExit.Image")));
+            this.BtnExit.Location = new System.Drawing.Point(300, 0);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(50, 40);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.UseVisualStyleBackColor = true;
             // 
             // PnlHistory
             // 
@@ -62,13 +80,26 @@ namespace CalculadoraWindows
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(300, 0);
+            this.button1.Location = new System.Drawing.Point(250, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(50, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "BtnExit";
+            this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(200, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 40);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -91,8 +122,10 @@ namespace CalculadoraWindows
         #endregion
 
         private System.Windows.Forms.Panel PnlTitle;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnExit;
         private System.Windows.Forms.Panel PnlHistory;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
